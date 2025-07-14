0b6f7a859be5b532f0d0e4a88bdfebf4e6e3629d
cw7430 <cw282@naver.com>
2025-07-14 11:08:08 +0900

    이상윤 과제

--- src/main/java/org/example/basic/sangyoonlee/unitone/Book.java
+++ src/main/java/org/example/basic/sangyoonlee/unitone/Book.java
@@ -6,23 +6,45 @@ public class Book {
     private String isbn;
     private boolean isBorrowed;
 
-    public Book() {
-
-    }
-
-    public Book(String title, String author, String isbn, boolean isBorrowed) {
+    public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
-        this.isBorrowed = isBorrowed;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
     }
 
     public void borrowBook() {
-        isBorrowed = true;
+        if (!isBorrowed) {
+            this.isBorrowed = true;
+            System.out.println("'" + title + "' (이)가 대여되었습니다.");
+        } else {
+            System.out.println("'" + title + "' (은)는 이미 대여 중입니다.");
+        }
     }
 
     public void returnBook() {
-        isBorrowed = false;
+        if (isBorrowed) {
+            this.isBorrowed = false;
+            System.out.println("'" + title + "' (이)가 반납되었습니다.");
+        } else {
+            System.out.println("'" + title + "' (은)는 대여 중인 책이 아닙니다.");
+        }
     }
 
     @Override
