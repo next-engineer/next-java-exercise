3d7b40a5a9ae43f72596cf40ae639cb2c685fb3b
hyeminleeee <wds5329@naver.com>
2025-07-14 11:16:34 +0900

    (feat) unit1 과제 진행 중

--- src/main/java/org/example/basic/leehyemin/UnitOne/Member.java
+++ src/main/java/org/example/basic/leehyemin/UnitOne/Member.java
@@ -8,6 +8,31 @@ public class Member {
     public String memberId;
     public ArrayList<Book> borrowedBooks;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberId() {
+        return this.memberId;
+    }
+
+    public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return this.borrowedBooks;
+    }
+
+    //메소드
     public void borrow(Book book) {
 
     }
