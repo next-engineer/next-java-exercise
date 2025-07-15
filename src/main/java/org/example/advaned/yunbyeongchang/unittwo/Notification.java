13af6c5099f4ae336b0fcd9ebd3e9fcb43102bca
ByeongChang <like98100@naver.com>
2025-07-15 17:17:39 +0900

    심화 과제 level1(기본 구현)

---
+++ src/main/java/org/example/advaned/yunbyeongchang/unittwo/Notification.java
@@ -0,0 +1,12 @@
+package org.example.advaned.yunbyeongchang.unittwo;
+
+public class Notification {
+    //var
+    
+    
+    //method
+    public void send()
+    {
+        System.out.println("이메일 전송 완료");
+    }
+}
