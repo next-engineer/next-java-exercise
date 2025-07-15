b94f68a8494e65b207cd50afaab2cef49f5f2c17
ByeongChang <like98100@naver.com>
2025-07-15 17:45:17 +0900

    심화 과제 level4(추가 확장).01
    아직 오류가 있어 추가 수정 필요

--- src/main/java/org/example/advaned/yunbyeongchang/unittwo/Main.java
+++ src/main/java/org/example/advaned/yunbyeongchang/unittwo/Main.java
@@ -1,12 +1,12 @@
 package org.example.advaned.yunbyeongchang.unittwo;
 
 public class Main {
+
     public static void main(String[] args)
     {
-        SMSNotification smsNoti = new SMSNotification();
-        EMailNotification emailNoti = new EMailNotification();
+        SelectNotification selectNoti = new SelectNotification();
 
-        smsNoti.send();
-        emailNoti.send();
+        selectNoti.SelectMenu();
+
     }
 }
---
+++ src/main/java/org/example/advaned/yunbyeongchang/unittwo/SelectNotification.java
@@ -0,0 +1,40 @@
+package org.example.advaned.yunbyeongchang.unittwo;
+
+import java.util.Scanner;
+
+public class SelectNotification {
+    //var
+    private EMailNotification emailNoti;
+    private SMSNotification smsNoti;
+    private PushNotification pushNoti;
+
+    //method
+    public void SelectMenu()
+    {
+        System.out.println("--------------------");
+        System.out.println("메뉴를 선택해 주세요.");
+        System.out.println("1. 이메일 알림");
+        System.out.println("2. SMS 알림");
+        System.out.println("3. 푸시 알림");
+        System.out.println("--------------------");
+        Scanner sc = new Scanner(System.in);
+        int inputValue = sc.nextInt();
+
+        switch (inputValue)
+        {
+            case 1:
+                emailNoti.send();
+                break;
+            case 2:
+                smsNoti.send();
+                break;
+            case 3:
+                pushNoti.send();
+                break;
+            default:
+                System.out.println("잘못된 입력입니다.");
+        }
+
+    }
+
+}
