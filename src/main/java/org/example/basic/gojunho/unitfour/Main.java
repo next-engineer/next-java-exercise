a9c9476ee3a081f5a8836a1cdd12499d0e2e554a
gogogojun <123915210+gogogojun@users.noreply.github.com>
2025-07-16 20:01:43 +0900

    Update Main.java

--- src/main/java/org/example/basic/gojunho/unitfour/Main.java
+++ src/main/java/org/example/basic/gojunho/unitfour/Main.java
@@ -4,17 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]){
-        Student student1 = new Student("두팔",90);
-        Student student2 = new Student("광팔",70);
-        Student student3 = new Student("공팔",80);
-        Student student4 = new Student("종팔",100);
+        Student student1 = new Student("두팔",0);
+        Student student2 = new Student("광팔",0);
+        Student student3 = new Student("공팔",0);
+        Student student4 = new Student("종팔",0);
         int sum=0;
         List<Student> studentList= Arrays.asList(student1,student2,student3,student4);
         Scanner sc = new Scanner(System.in);
         for(Student class1 : studentList){
             System.out.print(class1.getName()+"이의 점수를 입력하세요: ");
             class1.setScore(sc);
-            //System.out.println(class1.getName()+"이는 "+class1.getScore()+"점이야");
             class1.getHighScore();
             sum+=class1.getScore();
 
