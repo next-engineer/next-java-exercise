b07dd5db5f4b16505e2095ad75417b5277401832
MinDongIl <ehddlf9685@gmail.com>
2025-07-18 00:18:36 +0900

    advaned unitthree 수정

--- src/main/java/org/example/advaned/mindongil/unitthree/SmartSpeaker.java
+++ src/main/java/org/example/advaned/mindongil/unitthree/SmartSpeaker.java
@@ -1,4 +1,19 @@
 package org.example.advaned.mindongil.unitthree;
 
-public class SmartSpeaker {
+public class SmartSpeaker implements PowerControllable, MusicPlayable{
+    @Override
+    public void turnOn(){
+
+    }
+
+    @Override
+    public void turnOff(){
+
+    }
+
+    @Override
+    public void playMusic(String song) {
+
+    }
+
 }
