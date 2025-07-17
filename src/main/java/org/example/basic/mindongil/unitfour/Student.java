c5856a027e1b4a05fdb29c2de5670d33e9be0d80
MinDongIl <ehddlf9685@gmail.com>
2025-07-18 00:32:20 +0900

    feat: basic unitfive

--- src/main/java/org/example/basic/mindongil/unitfour/Student.java
+++ src/main/java/org/example/basic/mindongil/unitfour/Student.java
@@ -1,4 +1,31 @@
 package org.example.basic.mindongil.unitfour;
 
 public class Student {
+
+    private String studentName;
+    private int studentScore;
+
+    Student(String name, int score) {
+        this.studentName = name;
+        this.studentScore = score;
+    }
+
+    public int getStudentScore() {
+        return studentScore;
+    }
+
+    //고득점 출력
+    public void getHighScore() {
+        if(studentScore >= 90){
+            System.out.println("고득점자 : " + studentName);
+        }
+    }
+
+    //학생 정보 출력
+    public void printStudentInfo() {
+        System.out.println("이름: " + studentName);
+        System.out.println("점수: " + studentScore);
+        System.out.println();
+    }
+
 }
