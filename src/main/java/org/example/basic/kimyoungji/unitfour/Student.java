7ea5b6a5291b4f320337d2fc9e4de3c4bdc33dfc
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-17 22:07:30 +0900

    (chore) unitfour Student

---
+++ src/main/java/org/example/basic/kimyoungji/unitfour/Student.java
@@ -0,0 +1,31 @@
+package org.example.basic.kimyoungji.unitfour;
+
+public class Student {
+    private String name;
+    private int score;
+
+    // 생성자
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    // 점수 출력
+    public void printInfo() {
+        System.out.println("이름: " + name + ", 점수: " + score);
+    }
+
+    // 고득점 여부 반환
+    public boolean isHighScore() {
+        return score >= 90;
+    }
+
+    // Getter
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
