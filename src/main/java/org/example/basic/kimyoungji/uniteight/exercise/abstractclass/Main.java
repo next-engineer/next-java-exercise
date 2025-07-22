27fdeada48ba513cdf2a174a733202d54dae1b70
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-22 22:56:44 +0900

    (feat) uniteight abstractclass example complete

---
+++ src/main/java/org/example/basic/kimyoungji/uniteight/exercise/abstractclass/Main.java
@@ -0,0 +1,10 @@
+package org.example.basic.kimyoungji.uniteight.exercise.abstractclass;
+
+// 실행용 메인 클래스
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound();  // 출력: Woof!
+        myDog.sleep();      // 출력: Animal is sleeping...
+    }
+}
