066d564be7c4e65f3cbee9ad669b9f4d69f60da9
imjwoo <jeongu8807@daum.com>
2025-07-23 10:09:32 +0900

    basic eight

---
+++ src/main/java/org/example/basic/limjeongwoo/uniteight/Circle.java
@@ -0,0 +1,14 @@
+package org.example.basic.limjeongwoo.uniteight;
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
