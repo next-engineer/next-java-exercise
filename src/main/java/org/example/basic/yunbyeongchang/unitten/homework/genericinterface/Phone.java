04687541e15b7884be48588d3767538fbd018b2e
ByeongChang <like98100@naver.com>
2025-07-24 20:29:48 +0900

    기본 과제10 level4(Generic + Interface 활용한 PriceCalculator Class 생성)

---
+++ src/main/java/org/example/basic/yunbyeongchang/unitten/homework/genericinterface/Phone.java
@@ -0,0 +1,8 @@
+package org.example.basic.yunbyeongchang.unitten.homework.genericinterface;
+
+public class Phone implements Product{
+    //var
+
+    //method
+    public double getPrice() { return 999000.0; }
+}
