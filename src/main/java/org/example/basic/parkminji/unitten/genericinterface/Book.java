e6e92294bc780e02b4182213c64961e670bd42c7
Mminzy22 <aw7022@gmail.com>
2025-07-24 22:10:04 +0900

    feat: 제네릭 + 인터페이스 기반 가격 계산기 구현

---
+++ src/main/java/org/example/basic/parkminji/unitten/genericinterface/Book.java
@@ -0,0 +1,8 @@
+package org.example.basic.parkminji.unitten.genericinterface;
+
+public class Book implements Product {
+    @Override
+    public double getPrice() {
+        return 12000.0;
+    }
+}
