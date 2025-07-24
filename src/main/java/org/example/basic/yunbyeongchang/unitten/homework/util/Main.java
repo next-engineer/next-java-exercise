324f5d17983a7bf474bdde1158764911a8144853
ByeongChang <like98100@naver.com>
2025-07-24 20:14:53 +0900

    기본 과제10 level2(printAll Generic Method 생성)

---
+++ src/main/java/org/example/basic/yunbyeongchang/unitten/homework/util/Main.java
@@ -0,0 +1,15 @@
+package org.example.basic.yunbyeongchang.unitten.homework.util;
+
+public class Main {
+    //var
+
+    //method
+    public static void main(String[] args)
+    {
+        String[] names = {"짱구", "철수", "유리", "훈이", "맹구"};
+        Integer[] nums = {1, 2, 3, 4, 5};
+
+        Util.printAll(names);
+        Util.printAll(nums);
+    }
+}
