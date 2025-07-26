33006e28515ceb96d72ccf06e1ec078c69335032
ByeongChang <like98100@naver.com>
2025-07-26 18:05:48 +0900

    기본 과제11 level4(사용자 입력 기반 리스트 예제 구현)

---
+++ src/main/java/org/example/basic/yunbyeongchang/uniteleven/homework/uibasedlist/Main.java
@@ -0,0 +1,12 @@
+package org.example.basic.yunbyeongchang.uniteleven.homework.uibasedlist;
+
+public class Main {
+    //var
+
+    //method
+    public static void main(String[] args)
+    {
+        PrintMenu menu = new PrintMenu();
+        while(true) menu.selectMenu();
+    }
+}
