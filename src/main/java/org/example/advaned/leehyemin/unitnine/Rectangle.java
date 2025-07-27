3943aa3bc6e7115e17216fbb3d692674f6ade863
hyeminleeee <wds5329@naver.com>
2025-07-28 00:48:35 +0900

    (feat)0725 ProtoType 과제

---
+++ src/main/java/org/example/advaned/leehyemin/unitnine/Rectangle.java
@@ -0,0 +1,24 @@
+package org.example.advaned.leehyemin.unitnine;
+
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle "+width+"x"+height+" with color "+getColor());
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
