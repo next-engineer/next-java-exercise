5132e7e9454d7ab2ebfbd6960e169b9368e7a5f3
seleSeung <lomeme0114@gmail.com>
2025-07-28 18:33:56 +0900

    feat. unittwelve uniteight

--- src/main/java/org/example/basic/leeseungyeon/uniteight/Main.java
+++ src/main/java/org/example/basic/leeseungyeon/uniteight/Main.java
@@ -2,11 +2,10 @@ package org.example.basic.leeseungyeon.uniteight;
 
 public class Main {
     public static void main(String[] args) {
-        // 다형성 활용
-        Shape s1 = new Rectangle(4, 5);  // 직사각형의 면적: 20
-        Shape s2 = new Circle(3);        // 원의 면적: 28.27...
+        Shape s1 = new Rectangle(4, 5);
+        Shape s2 = new Circle(3);
 
-        System.out.println("Rectangle 면적: " + s1.area());
-        System.out.println("Circle 면적: " + s2.area());
+        System.out.println("사각형의 면적: " + s1.area());
+        System.out.println("원의 면적: " + s2.area());
     }
 }
