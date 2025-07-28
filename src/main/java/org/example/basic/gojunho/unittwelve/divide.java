e76479f8d9009e7b40d65f154d9a4dea7fdf6cad
gogogojun <rhwnsgh11@naver.com>
2025-07-28 19:39:08 +0900

    (feat) unittwelve complete

--- src/main/java/org/example/basic/gojunho/unittwelve/divide.java
+++ src/main/java/org/example/basic/gojunho/unittwelve/divide.java
@@ -7,9 +7,9 @@ public class divide {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int quotient = sc.nextInt();
-        int remainder = sc.nextInt();
+        int divisor = sc.nextInt();
         try {
-            int result = quotient/remainder;
+            int result = quotient/divisor;
         }catch (ArithmeticException e){
             System.out.println("예외발생: "+e.getMessage());
         }finally {
