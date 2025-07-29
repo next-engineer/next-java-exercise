9ceb3ee1a4eaf080d5827f91cab62307d1d0b5dd
ByeongChang <like98100@naver.com>
2025-07-29 20:04:54 +0900

    심화 과제11 level0(Decorator Pattern 예제 공부)

---
+++ src/main/java/org/example/advaned/yunbyeongchang/uniteleven/example/BasicCoffee.java
@@ -0,0 +1,14 @@
+package org.example.advaned.yunbyeongchang.uniteleven.example;
+
+public class BasicCoffee implements Coffee{
+    //var
+
+    //method
+    public String getDescription() {
+        return "Basic Coffee";
+    }
+
+    public int cost() {
+        return 3000;
+    }
+}
