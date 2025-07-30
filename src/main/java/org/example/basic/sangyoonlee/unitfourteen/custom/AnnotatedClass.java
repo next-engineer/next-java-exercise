c5b65a486426a1deede3901235ea7970199ad5a3
cw7430 <cw282@naver.com>
2025-07-30 23:31:21 +0900

    이상윤 2025-07-30 과제

---
+++ src/main/java/org/example/basic/sangyoonlee/unitfourteen/custom/AnnotatedClass.java
@@ -0,0 +1,8 @@
+package org.example.basic.sangyoonlee.unitfourteen.custom;
+
+public class AnnotatedClass {
+    @TestInfo(author = "Jaden", date = "2025-07-30")
+    public void sampleTest() {
+        System.out.println("Running sample test...");
+    }
+}
