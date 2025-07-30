478e6a1e47724eb2bbd28bd494eaa257842cfe76
Mminzy22 <aw7022@gmail.com>
2025-07-30 22:17:44 +0900

    feat: @TestInfo 어노테이션 적용된 테스트 메서드 클래스 구현

---
+++ src/main/java/org/example/basic/parkminji/unitfourteen/AnnotatedClass.java
@@ -0,0 +1,10 @@
+package org.example.basic.parkminji.unitfourteen;
+
+
+public class AnnotatedClass {
+
+    @TestInfo(author = "Jaden", date = "2025-07-30")
+    public void runTest() {
+        System.out.println("Running sample test...");
+    }
+}
