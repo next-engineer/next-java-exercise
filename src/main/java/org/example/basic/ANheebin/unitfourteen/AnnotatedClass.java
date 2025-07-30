41cd0b0133cbc94de1630c077309c1bc482cec0b
anheebin <anheebin@example.com>
2025-07-30 21:07:51 +0900

    Create AnnotatedClass.java

---
+++ src/main/java/org/example/basic/ANheebin/unitfourteen/AnnotatedClass.java
@@ -0,0 +1,9 @@
+package org.example.basic.ANheebin.unitfourteen;
+
+public class AnnotatedClass {
+
+    @TestInfo(autjor ="Jaden", date="2025-07-30")
+    public void sampleTest(){
+        System.out.println("Running sample test...");
+}
+}
