1208c554334634b3cfd0a2820e9b6cfa5c1fd12e
imjwoo <jeongu8807@daum.com>
2025-07-31 10:02:22 +0900

    basic fourteen

---
+++ src/main/java/org/example/basic/limjeongwoo/unitfourteen/AnnotatedClass.java
@@ -0,0 +1,8 @@
+package org.example.basic.limjeongwoo.unitfourteen;
+
+public class AnnotatedClass {
+    @TestInfo(author = "Jaden", date = "2025-07-30")
+    public void sampleTest() {
+        System.out.println("Running sample test...");
+    }
+}
