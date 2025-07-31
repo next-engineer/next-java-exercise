0da9e8098557392cc3b19961079766211b626cb2
seleSeung <lomeme0114@gmail.com>
2025-07-31 19:26:50 +0900

    feat. unitfifteen

---
+++ src/main/java/org/example/basic/leeseungyeon/unitfifteen/Main.java
@@ -0,0 +1,15 @@
+package org.example.basic.leeseungyeon.unitfifteen;
+
+import org.example.basic.leeseungyeon.unitfifteen.ListExample;
+import org.example.basic.leeseungyeon.unitfifteen.SetExample;
+import org.example.basic.leeseungyeon.unitfifteen.MapExample;
+import org.example.basic.leeseungyeon.unitfifteen.QueueExample;
+
+public class Main {
+    public static void main(String[] args) {
+        new ListExample().run();
+        new SetExample().run();
+        new MapExample().run();
+        new QueueExample().run();
+    }
+}
