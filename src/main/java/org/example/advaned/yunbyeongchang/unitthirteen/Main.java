c6998c4162ac05fa434c3525e94e6cfef511c195
ByeongChang <like98100@naver.com>
2025-07-31 20:02:41 +0900

    심화 과제13 level0(Composite 예제 공부)

---
+++ src/main/java/org/example/advaned/yunbyeongchang/unitthirteen/Main.java
@@ -0,0 +1,24 @@
+package org.example.advaned.yunbyeongchang.unitthirteen;
+
+public class Main {
+    //var
+
+    //method
+    public static void main(String[] args) {
+        FileComponent file1 = new File("hello.txt");
+        FileComponent file2 = new File("readme.md");
+        FileComponent file3 = new File("logo.png");
+
+        Directory imgFolder = new Directory("images");
+        imgFolder.add(file3);
+
+        Directory root = new Directory("root");
+        root.add(file1);
+        root.add(file2);
+        root.add(imgFolder);
+
+        root.showInfo("");
+
+    }
+
+}
