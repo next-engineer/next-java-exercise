d9945963f552de307c3dce17ba267e00aba60dc1
cw7430 <cw282@naver.com>
2025-08-05 16:53:44 +0900

    이2025-08-05 이상윤 과제

---
+++ src/main/java/org/example/advaned/sangyoonlee/unitfourteen/strategy/RateDiscount.java
@@ -0,0 +1,7 @@
+package org.example.advaned.sangyoonlee.unitfourteen.strategy;
+
+public class RateDiscount {
+    public int calculateDiscount(int price) {
+        return (int) (price * 0.9);
+    }
+}
