ceabff3671f937ecb00670212a539b429592fdfb
ByeongChang <like98100@naver.com>
2025-08-06 17:22:06 +0900

    기본 과제18 level2(Price 추가)

--- src/main/java/org/example/basic/yunbyeongchang/uniteightteen/CoffeeOrder.java
+++ src/main/java/org/example/basic/yunbyeongchang/uniteightteen/CoffeeOrder.java
@@ -10,7 +10,14 @@ public class CoffeeOrder {
     }
 
     public void printOrder() {
+        double priceResult = getPriceResult(type.getPrice(), size.getPriceMultiplier());
+
         System.out.println("주문하신 커피는 " + size + " 사이즈 " + type + "입니다.");
         System.out.println("용량: " + size.getMl() + "ml");
+        System.out.println("가격: " + priceResult + "KRW");
+    }
+
+    public double getPriceResult(double price, double priceMultiplier) {
+        return price * priceMultiplier;
     }
 }
--- src/main/java/org/example/basic/yunbyeongchang/uniteightteen/CoffeeType.java
+++ src/main/java/org/example/basic/yunbyeongchang/uniteightteen/CoffeeType.java
@@ -1,5 +1,15 @@
 package org.example.basic.yunbyeongchang.uniteightteen;
 
 public enum CoffeeType {
-    AMERICANO, LATTE, CAPPUCCINO
+    AMERICANO(1000), LATTE(2000), CAPPUCCINO(3000);
+
+    private final double price;
+
+    CoffeeType(int price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
