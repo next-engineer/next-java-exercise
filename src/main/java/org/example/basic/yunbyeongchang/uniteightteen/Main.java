35ac832725e0b99796b2d42cde8016dbd2f5a8a0
ByeongChang <like98100@naver.com>
2025-08-06 20:13:02 +0900

    기본 과제18 level3(IllegalArgumentException 추가)

--- src/main/java/org/example/basic/yunbyeongchang/uniteightteen/Main.java
+++ src/main/java/org/example/basic/yunbyeongchang/uniteightteen/Main.java
@@ -6,12 +6,26 @@ public class Main {
     //method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        CoffeeType type = null;
+        CoffeeSize size = null;
 
-        System.out.println("커피 종류를 선택하세요: AMERICANO, LATTE, CAPPUCCINO");
-        CoffeeType type = CoffeeType.valueOf(sc.next().toUpperCase());
+        try {
+            System.out.println("커피 종류를 선택하세요: AMERICANO, LATTE, CAPPUCCINO");
+            type = CoffeeType.valueOf(sc.next().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다. 프로그램을 종료합니다.");
+            sc.close();
+            return;
+        }
 
-        System.out.println("사이즈를 선택하세요: SMALL, MEDIUM, LARGE");
-        CoffeeSize size = CoffeeSize.valueOf(sc.next().toUpperCase());
+        try {
+            System.out.println("사이즈를 선택하세요: SMALL, MEDIUM, LARGE");
+            size = CoffeeSize.valueOf(sc.next().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다. 프로그램을 종료합니다.");
+            sc.close();
+            return;
+        }
 
         CoffeeOrder order = new CoffeeOrder(type, size);
         order.printOrder();
